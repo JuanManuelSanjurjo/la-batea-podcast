8286bab08ad784eb2e0071441e39e90e2f3b1339
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2024-11-05 12:40:15 -0300

    pagefind integration

---
+++ src/components/Player.jsx
@@ -0,0 +1,12 @@
+export default function Player() {
+  return (
+    <iframe
+      src="https://open.spotify.com/embed/show/0kQpgiacriTrYFQO5DASol?utm_source=generator"
+      width="100%"
+      height="352"
+      frameBorder="0"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
+  );
+}
