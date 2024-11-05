973493c70569c4805f4b827b2ac830a4890c17f1
JuanManuelSanjurjo <juanmanuelsanjurjo@gmail.com>
2024-11-05 13:26:29 -0300

    updated CONSTS: last episode and show

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,7 +1,9 @@
+import { LAST_EPISODE, PODCAST_SHOW } from "../consts";
+
 export default function Player() {
   return (
     <iframe
-      src="https://open.spotify.com/embed/show/0kQpgiacriTrYFQO5DASol?utm_source=generator"
+      src={LAST_EPISODE}
       width="100%"
       height="352"
       frameBorder="0"
--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,7 @@
 
 export const SITE_TITLE = "La Batea";
 export const SITE_DESCRIPTION = "La Batea Podcast";
+export const PODCAST_SHOW =
+  "https://open.spotify.com/embed/show/0kQpgiacriTrYFQO5DASol?utm_source=generator";
+export const LAST_EPISODE =
+  "https://open.spotify.com/embed/episode/408Kow0oB9itxme6CUnADi?utm_source=generator";
